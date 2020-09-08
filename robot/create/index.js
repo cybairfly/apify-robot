411ba99724b2ef3144ec4e3fc8cf6b08b6149f05
Tobey <cyberfly@email.cz>
2020-09-08 13:08:05 +0200

    update option creation & merging

--- robot/create/index.js
+++ robot/create/index.js
@@ -21,15 +21,6 @@ const CustomError = ({name = 'CustomError', data = {}, message = 'Custom Error'}
 };
 
 const Options = ({input, setup, INPUT: {block, stream, proxyConfig}}) => {
-    //already merged in setup
-    // const CUSTOM_OPTIONS = {
-    //     global: setup.OPTIONS || {},
-    //     target: tryRequire.global(setup.getPath.targets.setup(target)) || {},
-    // };
-
-    // const OPTIONS = R.mergeDeepRight(R.mergeDeepRight(DEFAULT_OPTIONS, CUSTOM_OPTIONS.global), CUSTOM_OPTIONS.target);
-    const {OPTIONS} = setup;
-
     const apifyProxySession = Apify.isAtHome() ?
         setup.getApifyProxySession.apify({input}) :
         setup.getApifyProxySession.local({input});
@@ -43,7 +34,7 @@ const Options = ({input, setup, INPUT: {block, stream, proxyConfig}}) => {
             .join('//')
         : proxyUrlWithoutSession;
 
-    const options = {
+    const defaultOptions = {
         launchPuppeteer: {
             useApifyProxy: proxyConfig ? proxyConfig.useApifyProxy : true,
             apifyProxyGroups: proxyConfig ? proxyConfig.apifyProxyGroups : undefined,
@@ -52,10 +43,7 @@ const Options = ({input, setup, INPUT: {block, stream, proxyConfig}}) => {
                 width: 1024 + Math.floor(Math.random() * 900),
                 height: 768 + Math.floor(Math.random() * 300)
             },
-            headless: Apify.isAtHome() ? OPTIONS.launchPuppeteer.headless : false,
-            // stealth: true,
-            // useChrome: true,
-            stealthOptions: OPTIONS.launchPuppeteer.stealth,
+            headless: Apify.isAtHome() ? setup.OPTIONS.launchPuppeteer.headless : false,
             devtools: !Apify.isAtHome(),
             // ignoreHTTPSErrors: true
             // args: [
@@ -64,7 +52,9 @@ const Options = ({input, setup, INPUT: {block, stream, proxyConfig}}) => {
         }
     };
 
-    if (!OPTIONS.launchPuppeteer.keepUserAgent) {
+    const options = R.mergeDeepRight(R.mergeDeepRight(DEFAULT_OPTIONS, defaultOptions), setup.OPTIONS);
+
+    if (options.launchPuppeteer.randomUserAgent) {
         options.launchPuppeteer.userAgent = proxyConfig && proxyConfig.userAgent
             ? proxyConfig.userAgent
             : getUserAgent();
@@ -72,12 +62,6 @@ const Options = ({input, setup, INPUT: {block, stream, proxyConfig}}) => {
 
     if (proxyUrl)
         options.launchPuppeteer.proxyUrl = proxyUrl;
-
-    if (block)
-        options.blockRequests = OPTIONS.blockRequests;
-
-    if (stream)
-        options.liveViewServer = OPTIONS.liveViewServer;
 
     return options;
 };
