6b02426e7dc6ac4fc55c2b5f6f7d7021bddf8538
Tobey <cyberfly@email.cz>
2021-04-24 19:45:12 +0200

    update, redesign custom error model

--- robot/errors/robot.error.js
+++ robot/errors/robot.error.js
@@ -1,94 +1,127 @@
 /**
  * @typedef {import('./d').RobotErrorOptions} RobotErrorOptions
  */
+const SKIP_KEYS = ['name', 'type', 'data', 'error', 'retry'];
 
 /* eslint-disable lines-between-class-members */
 module.exports = class RobotError extends Error {
-    #name = '';
-    #type = '';
-    #message = '';
-    #error = null;
+    #data = null;
+    #type = null;
+    #cause = null;
     #retry = false;
 
     /**
-     * Custom errors for robot
+     * Custom robot error with additional properties
+     * Rethrow a wrapped error - Robot.Error({error})
+     * Rethrow as custom error - extend Robot.Error
+     *
+     * Both cases support custom properties unless
+     * overloaded by the child error outside its ctor
+     *
+     * Prefer using the pre-defined data property for
+     * any extraneous properties besides the defined.
+     *
+     * Log to print custom error or stringify to send
+     * all its extra properties outside of the robot.
      * @param {RobotErrorOptions} options
      */
     constructor(options = {}) {
         super(options.message);
-        this.#name = options.name;
+        this.name = options.name;
         this.#type = options.type;
-        this.#message = options.message;
 
-        if (options.error)
-            this.#error = options.error;
-
-        if (options.retry)
-            this.#retry = options.retry;
+        Object
+            .entries({
+                ...options.error,
+                ...options,
+            })
+            .filter(([key]) => !SKIP_KEYS
+                .some(item => key === item))
+            .forEach(([key, value]) => this[key] = value);
 
         if (Error.captureStackTrace)
             Error.captureStackTrace(this, this.constructor);
 
-        Object.entries(options).forEach(([key, value]) => this[key] = value);
+        if (options.error) {
+            if (options.error.data)
+                this.data = options.error.data;
+
+            if (this.constructor.name !== 'RobotError')
+                this.#cause = options.error;
+            else {
+                this.#cause = options.error.cause;
+                this.name = options.name ?
+                    `${options.name} ◄ ${options.error.name}` :
+                    options.error.name;
+
+                this.message = options.message ?
+                    `${options.message} ◄ ${options.error.message.split('\n', 1)[0]}` :
+                    options.error.message;
+
+                if (options.stack)
+                    this.stack = options.error.stack;
+
+                if (options.error.retry)
+                    this.#retry = options.error.retry;
+            }
+        }
+
+        // extend or overwrite cause error data maybe
+        if (options.data)
+            this.data = options.data;
+
+        // overwrite cause error retry flag maybe
+        if (options.retry !== undefined)
+            this.#retry = options.retry;
+
+        this.name = this.name || `Robot.Error${this.constructor.name !== 'RobotError' && `.${this.constructor.name}`}`;
     }
 
-    get name() {
-        return this.#name
-        || (this.constructor.name === 'RobotError' && ((this.error && this.error.name) || 'Robot.Error'))
-        || (this.constructor.name !== 'RobotError' && this instanceof RobotError && `Robot.Error.${this.constructor.name}`)
-        || (this.constructor.name.toLowerCase().includes('error') ? this.constructor.name : `${this.constructor.name}Error`);
-    }
-
-    get type() {
-        return this.#type || this.constructor.name;
-    }
-
-    get message() {
-        return this.#message || (this.constructor.name === 'RobotError' && this.error && this.error.message) || '';
-    }
-
-    get error() {
-        return this.#error || null;
-    }
-
-    get retry() {
-        return this.#retry || false;
-    }
-
-    set name(name) {
-        this.#name = this.name || name;
+    set data(data = {}) {
+        if (typeof data !== 'object') return;
+        this.#data = {...this.#data, ...data};
+    } get data() {
+        return this.#data || null;
     }
 
     set type(type) {
         this.#type = this.type || type;
+    } get type() {
+        return this.#type || this.constructor.name;
     }
 
-    set message(message) {
-        this.#message = this.message || message;
-    }
-
-    set error(error) {
-        return this.#error || error;
+    set cause(error) {
+        this.#cause = this.#cause || error;
+    } get cause() {
+        return this.#cause || null;
     }
 
     set retry(retry) {
-        return this.#retry || retry;
+        this.#retry = this.#retry || retry;
+    } get retry() {
+        return this.#retry || false;
     }
 
-    toJSON = () => {
+    toJSON() {
         const output = {...this};
         output.name = this.name;
         output.type = this.type;
 
+        if (this.data)
+            output.data = this.data;
+
+        if (this.retry !== undefined)
+            output.retry = this.retry;
+
         if (this.message)
             output.message = this.message;
 
-        if (this.error)
-            output.error = this.error instanceof RobotError ? JSON.parse(JSON.stringify(this.error)) : this.error.toString();
-
-        if (this.retry)
-            output.retry = this.retry;
+        if (this.cause) {
+            output.cause = this.cause instanceof RobotError ?
+                JSON.parse(JSON.stringify(this.cause)) :
+                this.cause.toString();
+        }
 
         return Reflect.ownKeys(output).length ? output : null;
-    };
+    }
 };
