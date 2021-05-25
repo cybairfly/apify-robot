225a35ebdcef73a6948b94391ec123d9c408a997
Noah Chase <nchase@users.noreply.github.com>
2021-05-25 10:11:55 -0400

    refactor `formatMessage` so that it needs to take fewer inputs
    this makes the function more testable. errorLabel and errorDetails can be derived from the `error` and
    `details` arguments

--- robot/tools/notify/index.js
+++ robot/tools/notify/index.js
@@ -6,11 +6,8 @@ const { postMessage } = require('./slack');
 const notifyChannel = async ({input, error, options}) => {
     const {channels, details} = options.notify;
     const {channel} = channels.slack;
-    const {debug, target} = input;
 
-    const errorLabel = error.type || error.name || '';
-    const errorDetails = (debug || details) && JSON.stringify(error, null, 4);
-    const message = formatMessage({input, error, errorLabel, errorDetails});
+    const message = formatMessage({input, error, details});
 
     await postMessage({channel, message});
 };
@@ -29,10 +26,15 @@ const shouldExclude = (error, filters = {}) => Object
     .flatMap(filter => filter)
     .some(pattern => error.name === pattern || error.type === pattern);
 
-const formatMessage = ({input: {target, debug, session, stealth}, error, errorLabel, errorDetails}) => `
+const formatMessage = ({input: {target, debug, session, stealth}, error, details}) => {
+  const errorLabel = error.type || error.name || '';
+  const errorDetails = (debug || details) && JSON.stringify(error, null, 4);
+
+  return `
 Error: ${target} \`${errorLabel}\` ${stealth ? ':ninja:' : ''} ${debug ? ':ladybug:' : ''} ${session ? ':cookie:' : ''} ${error.retry ? ':recycle:' : ''}
 https://my.apify.com/view/runs/${process.env.APIFY_ACTOR_RUN_ID}${(errorDetails && `
 \`\`\`${errorDetails}\`\`\``) || ''}`.trim();
+};
 
 module.exports = {
     shouldNotify,
--- robot/tools/notify/notify.test.js
+++ robot/tools/notify/notify.test.js
@@ -8,8 +8,7 @@ describe('formatMessage', () => {
   })
   test('messages match expected format', () => {
     const error = new RobotError('generic error message'); 
-    const errorDetails = JSON.stringify(error, null, 4);
     const input = { target: 'hulu', debug: true, session: null, stealth: true }
-    expect(formatMessage({input, error, errorLabel: error.type || error.name, errorDetails })).toMatchSnapshot();
+    expect(formatMessage({input, error })).toMatchSnapshot();
   })
 });
