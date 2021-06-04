ab299e226ba2ce8442d4782dbc31193bce41b1b9
Tobey <cyberfly@email.cz>
2021-06-04 14:02:07 +0200

    handle missing groups in input proxy config

--- robot/tools/input/index.js
+++ robot/tools/input/index.js
@@ -2,6 +2,8 @@ const {InputOptions} = require('../options/index');
 
 // TODO clean up input
 const extendInput = async (input, setup) => {
+    input.proxyConfig = input.proxyConfig || {};
+    input.proxyConfig.groups = input.proxyConfig.groups || [];
     input.id = await setup.getInputId(input);
     input.options = InputOptions(input);
 
